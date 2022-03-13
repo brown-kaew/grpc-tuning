9818f86d81bb338ae82f10c18bf3602117f40449
brown-kaew <kaewtibkham@gmail.com>
2022-03-13 15:31:05 +0700

    init project

---
+++ src/main/java/io/brown/grpctuning/GrpcTuningApplication.java
@@ -0,0 +1,13 @@
+package io.brown.grpctuning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcTuningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcTuningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/brown/grpctuning/GrpcTuningApplicationTests.java
@@ -0,0 +1,13 @@
+package io.brown.grpctuning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcTuningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
